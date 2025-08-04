1587d3b5c864844bd91b279ad9f4d4d461ae5421
GemelliAngelo <gemeangelo27@gmail.com>
2025-08-04 11:29:37 +0200

    add method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,17 @@
 foreach (string nome in nomi)
 {
 Console.WriteLine(nome);
-    
 }
+
+static int Somma(int num1,int num2)
+{
+    return num1 + num2;
+}
+
+int number1 = int.Parse(Console.ReadLine());
+
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Somma(number1,number2));
 
 Console.Read();
