4bfa10b0a06d9cfb8522a1886d7962fb3547e999
Angelo Gemelli <angelo.gemelli@bitspa.it>
2025-09-18 17:07:25 +0200

    add comments

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -13,13 +13,13 @@
             List<int> nums = new();
 
             // Riempio la lista con un un numero casuale(min,max) di numeri casuali(min,max)
-            for(int i = 0; i < new Random().Next(2,10); i++)
+            for (int i = 0; i < new Random().Next(2, 10); i++)
             {
                 nums.Add(new Random().Next(100));
             }
 
             // Ciclo e sommo ogni elemento della lista
-            for(int i = 0; i < nums.Count; i++)
+            for (int i = 0; i < nums.Count; i++)
             {
                 Console.WriteLine(nums[i]);
                 sum += nums[i];
@@ -32,16 +32,19 @@
 
             #region Es b
 
+            // Dichiaro la somma
             int sum2 = 0;
 
             // Genero la lista con elementi inseriti via prompt convertiti in int
             List<int> nums2 = new([Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()),]);
 
-            for(int i = 0; i < nums2.Count; i++)
+            // Ciclo e sommo ogni elemento della lista
+            for (int i = 0; i < nums2.Count; i++)
             {
                 sum2 += nums2[i];
             }
 
+            // Stampo la somma
             Console.WriteLine($"La Somma è {sum2}");
 
             #endregion
