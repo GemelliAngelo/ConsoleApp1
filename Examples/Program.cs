f9cc637b4111f790c5299965c9bfac5780701586
Angelo Gemelli <angelo.gemelli@bitspa.it>
2025-09-18 16:49:54 +0200

    fix spacing

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -5,22 +5,27 @@
         static void Main(string[] args)
         {
             #region Es a
+
             // Dichiaro la somma
             int sum = 0;
+
             // Dichiaro la lista di numeri
             List<int> nums = new();
+
             // Riempio la lista con un un numero casuale(min,max) di numeri casuali(min,max)
             for(int i = 0; i < new Random().Next(2,10); i++)
             {
                 nums.Add(new Random().Next(100));
             }
+
             // Ciclo e sommo ogni elemento della lista
             for(int i = 0; i < nums.Count; i++)
             {
                 Console.WriteLine(nums[i]);
                 sum += nums[i];
             }
-            // Stampo la lista
+
+            // Stampo la somma
             Console.WriteLine($"La Somma è {sum}");
 
             #endregion
@@ -28,6 +33,7 @@
             #region Es b
 
             int sum2 = 0;
+
             // Genero la lista con elementi inseriti via prompt convertiti in int
             List<int> nums2 = new([Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()),]);
 
